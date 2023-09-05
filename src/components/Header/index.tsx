f0d8d193b52fae648347883a74a1a5ab7a1f1c3c
Davi Batista <davideosmar11@gmail.com>
2023-09-05 16:02:03 -0300

    feat: added header searchbar decor

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -38,14 +38,17 @@ const Header = (props: { children?: ReactNode }) => {
 
     return (
         <header className={styles.header}>
-            <Image
-                src={'/img/trattoria-logo-light.png'}
-                alt="Trattoria Fedeli"
-                width={170}
-                height={45}
-                className={styles.logo}
-            />
-            <input type="search" name="find" id="find" />
+            <Link href={'/table-menu'}>
+                <img
+                    src={'/img/trattoria-logo-light.png'}
+                    alt="Trattoria Fedeli"
+                    className={styles.logo}
+                />
+            </Link>
+            <div className={styles.searchBar}>
+                <input type="search" name="find" id="find" />
+                <Image src={'/header/search.svg'} alt="" width={25} height={25} className={styles.search}/>
+            </div>
             <div className={styles.menu} onClick={() => setModalClass(modalClass == styles.hidden ? styles.visible : styles.hidden)}>
                 <div className={styles.userButton}>
                     <Image src={'/header/person.svg'} width={25} height={25} alt='user-icon'/>
