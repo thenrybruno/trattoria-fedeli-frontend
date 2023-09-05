1ff1c9c449bd436fe4ece536c64160a8bd6a415d
Davi Batista <davideosmar11@gmail.com>
2023-09-05 13:25:54 -0300

    feat: added user command page

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -3,23 +3,25 @@
 import Header from '@/src/components/Header'
 import styles from './styles.module.scss'
 import { NextPage } from 'next'
-import { ReactNode } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import Footer from '@/src/components/common/Footer'
+import { ProductType } from '@/src/services/products'
 
+type CommandEntry = {
+    product: ProductType
+    quantity: number
+}
 
+const UserDashboard: NextPage = (props: { children?: ReactNode, entry?: CommandEntry }) => {
+    const [command, setCommand] = useState<ProductType[]>([])
 
-const UserDashboard: NextPage = (props: { children?: ReactNode }) => {
+    useEffect(() =>{
+        const storedCommand = localStorage.getItem('command')
 
-    //@ts-ignore
-    let tasks: [] = []
-
-    if(!localStorage.getItem('tasks'))  {
-        localStorage.setItem('tasks', JSON.stringify(tasks))
-    }   else    {
-
-    }
-
-    let localStorageData = window.localStorage.getItem('tasks')
+        if (storedCommand) {
+            setCommand(JSON.parse(storedCommand))
+        }
+    }, [])
 
 
     return (
@@ -29,13 +31,13 @@ const UserDashboard: NextPage = (props: { children?: ReactNode }) => {
                 <h1 className={styles.h1}>Meu pedido</h1>
                 <section className={styles.dashboard}>
                     {
-                        tasks.length < 1 ?
+                        command.length < 1 ?
                         <div className={styles.empty}>
                             <h1>Oh não! Você ainda não pediu nada. Que tal ver o cardápio?</h1>
                         </div>
                         :
                         <div>
-                            <h1>Hello, world!</h1>
+                            <CommandProduct/>
                         </div>
                     }
                 </section>
@@ -45,4 +47,97 @@ const UserDashboard: NextPage = (props: { children?: ReactNode }) => {
     )
 }
 
+const TableRow = (props: {
+    entry: CommandEntry
+})  =>  {
+    const [command, setCommand] = useState<ProductType[]>([])
+
+    useEffect(() =>{
+        const storedCommand = localStorage.getItem('command')
+
+        if (storedCommand) {
+            setCommand(JSON.parse(storedCommand))
+        }
+    }, [])
+
+    const removeProduct = (productId: number) =>{
+        const productIndex = command.findIndex(product => product.id === productId)
+
+        if (productIndex !== -1) {
+            const updatedCart = [...command]
+            updatedCart.splice(productIndex, 1)
+            localStorage.setItem('command', JSON.stringify(updatedCart))
+            setCommand(updatedCart)
+        }
+    }
+
+    return  (
+        <div className={styles.row}>
+            <span className={styles.divide} >
+                <img src={props.entry.product.imageUrl} alt={props.entry.product.name} className={styles.thumbnail} />
+            </span>
+            <span className={styles.divide}>
+                {props.entry.product.name}
+            </span>
+            <span className={styles.divide}>
+                {props.entry.quantity}
+            </span>
+            <span className={styles.divide}>
+            R$ {(props.entry.product.price * props.entry.quantity)}
+            </span>
+        </div>
+    )
+}
+
 export default UserDashboard
+
+export function CommandProduct()    {
+    const [ commandEntries, setCommandEntries ] = useState<CommandEntry[]>([])
+    const [command, setCommand] = useState<ProductType[]>([])
+
+    useEffect(() =>{
+        const storedCommand = localStorage.getItem('command')
+
+        if (storedCommand) {
+            setCommand(JSON.parse(storedCommand))
+        }
+    }, [])
+
+    useEffect(() => {
+        const entriesList = command.reduce((list, product) => {
+            const entryIndex = list.findIndex(entry => entry.product.id === product.id)
+
+            if (entryIndex === -1) {
+                return [
+                    ...list,
+                    {
+                        product,
+                        quantity: 1
+                    }
+                ]
+            }
+
+            list[entryIndex].quantity++
+            return list
+
+        }, [] as CommandEntry[])
+
+        entriesList.sort((a, b) => a.product.id - b.product.id)
+        setCommandEntries(entriesList)
+
+    }, [command])
+
+    return  (
+        <section>
+            <div className={styles.tr}>
+                <h6>Produto</h6>
+                <h6>Preço</h6>
+                <h6>Qtd.</h6>
+                <h6>Total</h6>
+            </div>
+            <div className={styles.products}>
+                {commandEntries.map(entry => <TableRow key={entry.product.id} entry={entry} />)}
+            </div>
+        </section>
+    )
+}
