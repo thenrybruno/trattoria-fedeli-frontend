d7d43287dbc6be9deb43d20b84b7518101b81b11
Davi Batista <davideosmar11@gmail.com>
2023-09-05 14:47:14 -0300

    feat: added parsing to user's name

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -25,6 +25,8 @@ const UserDashboard: NextPage = (props: { children?: ReactNode, entry?: CommandE
         }
     }, [])
 
+    
+
 
     return (
         <section className={styles.section}>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,32 @@
+'use client'
 import Image from 'next/image'
 import styles from './index.module.scss'
+import { ReactNode, useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 
-const Header = () => {
+const Header = (props: { children?: ReactNode }) => {
+    const [user, setUser] = useState<UserType>()
+
+    type UserType = {
+        name: string,
+        table: number,
+        cellphone: string,
+        atHome: boolean
+    }
+
+
+    const router = useRouter()
+
+    useEffect(() =>{
+        const storedUser = sessionStorage.getItem('user')
+
+        if (storedUser) {
+            setUser(JSON.parse(storedUser))
+        } else  {
+            router.push('/')
+        }
+    }, [user])
+
     return (
         <header className={styles.header}>
             <Image
@@ -13,7 +38,7 @@ const Header = () => {
             />
             <input type="search" name="find" id="find" />
             <div>
-                <p>Name</p>
+                <p>{user?.name}</p>
             </div>
         </header>
     )
